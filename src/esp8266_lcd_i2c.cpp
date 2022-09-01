dbaee3eb4f46a6f244fcee418ff64da8c47bef74
Tomas <duarte.t@es.ungs.edu.ar>
2022-09-01 13:02:28 -0300

    aaa

--- src/esp8266_lcd_i2c.cpp
+++ src/esp8266_lcd_i2c.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+String frase =" ";
+boolean frasecompleta = false;
 
 void setup()
 {
-   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  frase.reserve(200);
+  pinMode(LED_BUILTIN, OUTPUT);
   // Iniciar LCD
   //lcd.begin(20, 4);
   lcd.init();
@@ -14,15 +17,28 @@ void setup()
   lcd.clear();
   // Imprimir
   lcd.setCursor(0, 0);
-  lcd.print("hola mundo ");
+  lcd.print("hola maxy ");
   lcd.setCursor(0, 1); // Segunda fila
-  lcd.print("tecteach.net.ar ");
+
 }
 
-void loop()
-{
+void loop(){
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
+  if (frasecompleta){
+  lcd.println (frase);
+  frasecompleta = false;
+  frase = " ";
+}
+}
+
+
+void serialevent() {
+while (Serial.available()){
+  char caracter =(char) Serial.read();
+  frase+=caracter;
+  if(caracter=='\n') frasecompleta = true;
+}
 }
